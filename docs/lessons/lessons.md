66c092bd603cb478c0cdd6f6998e7c7b953c48a9
Daniel Banck <daniel@dbanck.de>
2022-03-29 22:47:37 +0200

    hide lessons for now

---
+++ docs/lessons/lessons.md
@@ -0,0 +1,7 @@
+---
+sidebar_class_name: none
+---
+
+# Lektionen
+
+Hier folgen nach jedem Kurstermin die Inhalte.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,24 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
+function filterSidebarItems(
+  /** @type {import('@docusaurus/plugin-content-docs/src/sidebars/types').NormalizedSidebar} */ items
+) {
+  if (process.env.NODE_ENV === "development") {
+    return items;
+  }
+
+  return items.map((item) => {
+    if (item.type === "category") {
+      return {
+        ...item,
+        items: item.items.filter((item) => item.className !== "hidden"),
+      };
+    }
+    return item;
+  });
+}
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Webprojekt",
@@ -22,10 +40,18 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          async sidebarItemsGenerator({
+            defaultSidebarItemsGenerator,
+            ...args
+          }) {
+            const sidebarItems = await defaultSidebarItemsGenerator(args);
+            return filterSidebarItems(sidebarItems);
+          },
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl: "https://github.com/dbanck/webprojekt/edit/main",
         },
+
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
