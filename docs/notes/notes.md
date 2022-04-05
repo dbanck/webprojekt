7d24189af772c81fef192474fa1f45be17af633d
Daniel Banck <daniel@dbanck.de>
2022-04-05 23:23:14 +0200

    add github code repo link

--- docs/notes/notes.md
+++ docs/notes/notes.md
@@ -3,6 +3,10 @@ title: Notizen
 sidebar_position: 1
 ---
 
+import ProminentLink from '@site/src/components/Docs/ProminentLink';
+
+<ProminentLink link="https://github.com/dbanck/webprojekt-sose22" style={{width: "20rem" }}>GitHub Projekt mit Code</ProminentLink>
+
 ## Allgemein
 
 - Umfangreiche Sammlung zum Nachschlagen zu allen Themen rund um die Webentwicklung: https://developer.mozilla.org/en-US/
--- src/components/Docs/ProminentLink.tsx
+++ src/components/Docs/ProminentLink.tsx
@@ -3,12 +3,14 @@ import React from "react";
 const ProminentLink = ({
   children,
   link,
+  style,
 }: {
   children: React.ReactNode;
   link: string;
+  style: any;
 }) => {
   return (
-    <a href={link} target="_blank" className="prominent-link">
+    <a href={link} target="_blank" className="prominent-link" style={style}>
       <span className="circle" aria-hidden="true">
         <span className="icon arrow"></span>
       </span>
