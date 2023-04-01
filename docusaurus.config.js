3a2bfea50c1c3533bcdcd4cb89c1ec28e23ca05a
Daniel Banck <daniel@dbanck.de>
2023-04-01 13:37:20 +0200

    update code url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           {
             position: "left",
             label: "GitHub Code",
-            href: "https://github.com/dbanck/webprojekt-wise22",
+            href: "https://github.com/dbanck/webprojekt-sose23",
             className: "header-github-link",
             "aria-label": "GitHub repository",
           },
